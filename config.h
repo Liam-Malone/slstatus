c93ed7ae7b362ee0ec4caedef0bb9982b109ef4f
Liam-Malone <liammaloneirlusa@gmail.com>
2022-08-15 01:32:37 -0400

    improving

--- config.h
+++ config.h
@@ -70,6 +70,7 @@ static const struct arg args[] = {
 	{ ram_perc, "| %s%%", NULL  },
 	{ netspeed_rx, "| %sB/s|", "ens33"  },
 	{ run_command, " %4s|", "amixer sget Master | awk -F\"[][]\" '/%/ { print $2 }' | head -n1" },
-	{ battery_perc, " %s%%|", "BAT1" },
+	{ battery_state, "%s", "BAT1" },
+	{ battery_perc, "%s%%|", "BAT1" },
 	{ datetime, "%s"," %a %d-%b  %T" },
 };
